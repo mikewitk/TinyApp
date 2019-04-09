9aba99d1efb7091b7a46c7f7a90e29940a477b49
Michael <mike.witk@gmail.com>
2019-04-09 23:09:03 +0000

    Form fully functional and printing the request. Package update to contain PARSE lib

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = 8080; //default port 8080
 
 app.set("view engine", "ejs")
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -13,16 +16,9 @@ app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
-// when the path matches the string below, the function is executed
-app.get('/urls.json', (req, res) => {
-  // the respond STREAM from the request is read until its completion.
-  res.json(urlDatabase);
-});
-
-// when the path matches the string below, the function is executed
-app.get('/hello', (req, res) => {
-  // the respond is now a HTML page
-  res.send('<html><body>Hello <b>World</b></body></html>\n')
+//route handler to render the form page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 //route handler for /urls to pass the URL data to our template
@@ -31,12 +27,48 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.longURL) ;  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 //route handler for /urls/:shorURL to pass the shortURL data to our template
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // when the path matches the string below, the function is executed
+// app.get('/urls.json', (req, res) => {
+//   // the respond STREAM from the request is read until its completion.
+//   res.json(urlDatabase);
+// });
+
+// // when the path matches the string below, the function is executed
+// app.get('/hello', (req, res) => {
+//   // the respond is now a HTML page
+//   res.send('<html><body>Hello <b>World</b></body></html>\n')
+// });
